600b25643d765749d350d3e8051a11e9315c68a5
ABgitme <abebekezhabesha@gmail.com>
2023-11-14 09:14:40 +0300

    test reading from file

---
+++ main.c
@@ -0,0 +1,43 @@
+#include "monty.h"
+
+/**
+* main - monty code interpreter
+* @argc: number of arguments
+* @argv: monty file location
+* Return: 0 on success
+*/
+int main(int argc, char *argv[])
+{
+	char *line;
+	FILE *file;
+	size_t size = 0;
+	ssize_t r_line = 0;
+	stack_t *stack = NULL;
+	unsigned int line_count = 0;
+
+NOTUSED(stack);
+	if (argc != 2)
+	{
+		fprintf(stderr, "USAGE: monty file\n");
+		exit(EXIT_FAILURE);
+	}
+	file = fopen(argv[1], "r");
+	if (file == NULL)
+	{
+		fprintf(stderr, "Error: Can't open file %s\n", argv[1]);
+		exit(EXIT_FAILURE);
+	}
+	while ((r_line = getline(&line, &size, file)) > 0)
+	{
+		line_count++;
+		if (r_line > 0)
+		{
+            printf("%s\n", line);
+		}
+		free(line);
+        line = NULL;
+	}
+	fclose(file);
+    printf("%d\n", line_count);
+return (0);
+}
