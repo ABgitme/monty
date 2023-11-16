577d8012385342cc8a04482f9be315e3e2a5ca6d
abel-alx-github <abeltibeso323@gmail.com>
2023-11-16 01:35:24 -0800

    multiplication

---
+++ handle_mul.c
@@ -0,0 +1,28 @@
+#include "monty.h"
+/**
+*_mul - multiply top two int of stack
+*@stack: pointer to pointer of head list
+*@line_number: line at which opcode is found
+*/
+void _mul(stack_t **stack, unsigned int line_number)
+{
+	int mul = 0;
+	stack_t *temp;
+
+	if (!stack || !(*stack) || !(*stack)->next)
+	{
+		fprintf(stderr, "L%d: can't add, stack too short\n", line_number);
+		free(glob_buffer.line_buf);
+		free_dlink(*stack);
+		exit(EXIT_FAILURE);
+	}
+	else
+	{
+		temp = *stack;
+		*stack = (*stack)->next;
+		mul = temp->n * (*stack)->n;
+		(*stack)->n = mul;
+		(*stack)->prev = NULL;
+		free(temp);
+	}
+}
--- run.c
+++ run.c
@@ -22,6 +22,7 @@ int run(char *line, FILE *file, stack_t **stack, unsigned int line_number)
 	{"nop", _nop},
 	{"sub", _sub},
 	{"div", _div},
+	{"mul", _mul},
 	{NULL, NULL}};
 
 	NOTUSED(file);
