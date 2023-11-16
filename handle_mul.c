5328a7ba75b50b32da5d755c7d411ac33afbe0e1
abel-alx-github <abeltibeso323@gmail.com>
2023-11-16 01:36:53 -0800

    multiplication

--- handle_mul.c
+++ handle_mul.c
@@ -11,7 +11,7 @@ void _mul(stack_t **stack, unsigned int line_number)
 
 	if (!stack || !(*stack) || !(*stack)->next)
 	{
-		fprintf(stderr, "L%d: can't add, stack too short\n", line_number);
+		fprintf(stderr, "L%d: can't mul, stack too short\n", line_number);
 		free(glob_buffer.line_buf);
 		free_dlink(*stack);
 		exit(EXIT_FAILURE);
